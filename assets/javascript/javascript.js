2b224e1a006bc894c6d6092e9c9633e961800a47
hatzi249 <58467708+hatzi249@users.noreply.github.com>
2020-04-28 11:59:10 +0200

    Update javascript.js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,3 +17,73 @@ function myFunction() {
     });
     var marker = new google.maps.Marker({ position: uluru, map: map });
   }
+
+/*Categories*/
+
+const div = document.getElementById("beef");
+
+ getMenu = async () => {
+  try {
+      const result = axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+      const { data: categories } = await result;
+      div.innerHTML="";
+      categories.categories.forEach(category => {
+          div.innerHTML +=
+          `
+            
+              <div class="card-categories">
+                <img
+                class="image-categories"
+                src="${category.strCategoryThumb}"
+              style="width: 100%;"
+              />
+          <div class="card-text-container">
+            <h3>${category.strCategory}</h3>
+            <p>${category.strCategoryDescription}</p>
+          </div>
+          
+       `;
+      }); 
+  }
+  catch (err) {
+      div.innerHTML = `Couldn´t reach the API.`
+      console.log("getCustomers: ERROR", err);
+  }
+}
+getMenu ();
+
+/*Search*/
+
+let dish = document.getElementById("dishName");
+let get = document.getElementById("myButton");
+ 
+
+get.onclick = async () => {
+  try {
+
+    let input = document.getElementById('myInput').value;
+    const result = axios.get (`https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`);
+      const { data: meals } = await result;
+      dish.innerHTML ="";
+      meals.meals.forEach(meal => {
+        dish.innerHTML +=
+        `
+          <div class="card-categories">
+          <img
+            class="image-categories"
+            src="${meal.strMealThumb}"
+            style="width: 100%;"
+          />
+          <div class="card-text-container">
+              <h3>${meal.strMeal}</h3>
+              <p>${meal.strInstructions}</p>
+          </div>
+        `;
+          
+      }); 
+  }
+  catch (err) {
+      dish.innerHTML = `Couldn´t reach the API.`
+      console.log("getCustomers: ERROR", err);
+  }
+}
